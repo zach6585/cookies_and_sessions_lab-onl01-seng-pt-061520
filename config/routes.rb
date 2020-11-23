6b26b8ea4ac8265e4956807dae6714ca84bda88b
zach6585 <zmines@wesleyan.edu>
2020-11-23 16:18:36 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/cart', to: "application#cart"
+  # get '/', to: "application#index"
+  post '/add', to: "products#add"
+  get '/', to: "products#index" 
 end
